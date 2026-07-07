53569ffca1465aa7bf6b13b57292ef5b39152a10
wtellos <rfiouris@gmail.com>
2026-07-07 17:40:44 +0300

    Added CRUD for Ninjas with Dojos and voting setup styling to delete Ninja button

--- resources/views/ninjas/show.blade.php
+++ resources/views/ninjas/show.blade.php
@@ -23,7 +23,7 @@
     @csrf
     @method('DELETE')
 
-    <button type="submit" class="btn my-4">Delete Ninja</button>
+    <button type="submit" class="btn my-4 mt-4 inline-flex items-center px-4 py-2 bg-red-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-red-500 active:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150">Delete Ninja</button>
   </form>
 </div>
 </x-app-layout>
